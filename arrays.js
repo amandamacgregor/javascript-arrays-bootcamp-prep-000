641b1179c981e32c988aa44c061afc0f0482a4d3
Amanda MacGregor <amandaseagull3@gmail.com>
2018-02-16 23:15:02 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+console.log(chocolateBars)
+function addElementToBeginningOfArray(array, element) {
+  var newArray = ["Foo", 1, ...chocolateBars]
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  chocolateBars.unshift('foo', 1)
+}
+function addElementToEndOfArray(array, element) {
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+}
+function accessElementInArray(array, index) {
+  return element
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
+function removeElementFromBeginningOfArray(array) {
+  
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  
+}
+function removeElementFromEndOfArray(array) {
+  
+}
